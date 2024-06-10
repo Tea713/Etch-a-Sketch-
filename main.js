c762b01b84b4b5ee918a9ba3d03c22d08939437e
Tea713 <thanhnguyen_2025@depauw.edu>
2024-06-11 00:14:36 +0700

    Add random colorr, increasing in opacity

--- main.js
+++ main.js
@@ -3,14 +3,19 @@ const canvasSide = 576;
 const container = document.querySelector("#container");
 
 function initiateGrid(squarePerSide) {
+    container.innerHTML = '';
     let sideOfSquare = canvasSide / squarePerSide;
     for (let j = 0; j < squarePerSide * squarePerSide; j++) {
         let grid = document.createElement("div");
         grid.setAttribute("class", "grid");
         grid.style.width = sideOfSquare + "px";
         grid.style.height = sideOfSquare + "px";
+        grid.style.opacity = 0.1;
         grid.addEventListener("mouseenter", () => {
-            grid.style.backgroundColor = "blue";
+            grid.style.backgroundColor = "#" + (Math.random() * 0xFFFFFF  << 0).toString(16).padStart(6, '0');
+            if(grid.style.opacity < 1) {
+                grid.style.opacity = ((parseFloat(grid.style.opacity)) + 0.1).toString();
+            }
         });
         grid.addEventListener("mouseleave", () => {
 
@@ -19,4 +24,12 @@ function initiateGrid(squarePerSide) {
     }
 }
 
-initiateGrid(64);
+initiateGrid(16);
+
+document.querySelector("#reset").addEventListener("click", () => {
+    let squarePerSide = window.prompt("How many squares per side would you like?");
+    while(squarePerSide < 1 || squarePerSide > 100) {
+        squarePerSide = window.prompt("Please enter a number between 1 and 100");
+    }   
+    initiateGrid(squarePerSide);
+});
