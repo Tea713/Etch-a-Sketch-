29c30ff11ce000e665602aa2707ad1a1f875dbfa
Tea713 <thanhnguyen_2025@depauw.edu>
2024-06-10 14:04:22 +0700

    Create dividing into grid logic

--- main.js
+++ main.js
@@ -2,23 +2,21 @@ const canvasSide = 576;
 
 const container = document.querySelector("#container");
 
-function initiateGrid() {
-    for(let j = 0; j < 16; j++) {
-        let row = document.createElement("div");
-        row.setAttribute("class", "row");
-        for(let i = 0; i < 16; i++) {
-            let grid = document.createElement("div");
-            grid.setAttribute("class", "grid");
-            grid.addEventListener("mouseenter", () => {
-                grid.style.backgroundColor = "blue";
-            });
-            grid.addEventListener("mouseleave", () => {
+function initiateGrid(squarePerSide) {
+    let sideOfSquare = canvasSide / squarePerSide;
+    for (let j = 0; j < squarePerSide * squarePerSide; j++) {
+        let grid = document.createElement("div");
+        grid.setAttribute("class", "grid");
+        grid.style.width = sideOfSquare + "px";
+        grid.style.height = sideOfSquare + "px";
+        grid.addEventListener("mouseenter", () => {
+            grid.style.backgroundColor = "blue";
+        });
+        grid.addEventListener("mouseleave", () => {
 
-            })
-            row.appendChild(grid);
-        }
-        container.appendChild(row);
+        })
+        container.appendChild(grid);
     }
 }
 
-initiateGrid();
+initiateGrid(64);
